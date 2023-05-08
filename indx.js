3d0686f3ccc6763a2655b52a16ec3fdaffc34dff
João Pedro <joaopsj007@gmail.com>
2023-05-08 17:09:11 -0300

    .

--- indx.js
+++ indx.js
@@ -1,12 +1,23 @@
 const iconsNav = document.querySelectorAll("#navBarList li a");
 
-function activeIconNav() {
+document.addEventListener("scroll", () => {
+  const scroll = scrollY;
   iconsNav.forEach(function (el) {
     el.classList.remove("actived");
   });
-  this.classList.add("actived");
-}
-iconsNav.forEach(function (el) {
-  el.addEventListener("click", activeIconNav);
-  // el.addEventListener("click", activeIconNav);
+
+  if (scroll < 570) {
+    document.querySelector("a[href='#home']").classList.add("actived");
+  } else if (scroll > 570 && scroll < 1141) {
+    document.querySelector("a[href='#aboutMe']").classList.add("actived");
+  } else if (scroll > 1141 && scroll < 1712) {
+    document.querySelector("a[href='#myExperience']").classList.add("actived");
+  } else if (scroll > 1712 && scroll < 2540) {
+    document.querySelector("a[href='#projects']").classList.add("actived");
+  } else if (scroll > 2540) {
+    document.querySelector("a[href='#contact']").classList.add("actived");
+  }
 });
+
+const a = [];
+console.log(a);
